a1759017a7392660dc78967840364419afbdf8c5
Daniel Martins Coutinho <danielmc__@hotmail.com>
2021-11-29 14:42:10 -0300

    index obrigando login

--- gestao/lib/gestao_web/controllers/page_controller.ex
+++ gestao/lib/gestao_web/controllers/page_controller.ex
@@ -1,5 +1,8 @@
 defmodule GestaoWeb.PageController do
+  import GestaoWeb.UsuarioAuth
   use GestaoWeb, :controller
+
+  plug :require_authenticated_usuario
 
   def index(conn, _params) do
     render(conn, "index.html")
